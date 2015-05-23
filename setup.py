cbb5bc69c0094409e1f088fbc95346658e5c80ca
Matteo Banerjee <matteobanerjee@gmail.com>
2015-05-23 10:56:54 -0400

    Add setup.py and license

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+# with open('requirements.txt') as requirements:
+#     reqs = requirements.read().splitlines()
+
+setup(
+    name='Collaborative Filtering Tools',
+    version='0.1',
+    description=('A library for collaborative filtering'
+                 'and similarity-based recommendations'),
+    author='Matteo Banerje',
+    author_email='matteobanerjee@gmail.com',
+    url='https://github.com/matteobanerjee/collaborative_filtering_tools',
+    # install_requires=reqs, <-- Currently can't get numpy to install this way
+    packages=['cf_tools'],
+    license='MIT'
+)
